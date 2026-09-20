7d59167d9e0dd93ecb581a4f41349059eba25c4d
systemkiller03 <271533435+systemkiller03@users.noreply.github.com>
2026-09-21 01:14:58 +0500

    feat: leaderboard set up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createHeader } from './features/header/header'
 import { createHero } from './features/hero/hero'
 import { createCarousel } from './features/carousel/carousel'
+import { createLeaderboard } from './features/leaderboard/leaderboard'
 import './styles/main.scss'
 
 const app: HTMLDivElement = document.createElement('div')
@@ -9,3 +10,4 @@ document.body.append(app)
 app.append(createHeader())
 app.append(createHero())
 app.append(createCarousel())
+app.append(createLeaderboard())
