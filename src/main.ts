47ff394301a1ec38b5bdc447f4cd761e36221569
systemkiller03 <271533435+systemkiller03@users.noreply.github.com>
2026-09-20 23:04:33 +0500

    feat: mobile burger menu set up

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { createHeader } from './features/header/header'
 import './styles/main.scss'
 
 const app: HTMLDivElement = document.createElement('div')
 app.id = 'app'
 document.body.append(app)
+app.append(createHeader())
